a058baf24a62efd593a5114262ad98d883eab12a
Berwin Singh <63018717+berwinsingh@users.noreply.github.co>
2023-08-09 22:20:21 +0530

    Created Further HTML

---
+++ JavaScript/newTask.js
@@ -0,0 +1,3 @@
+import { allTasks, highPriority,mediumPriority,lowPriority } from "./contentUpdate";
+
+const taskTemplate = document.getElementById("new-task");
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       dropShadow:{
-        '2xl': '1px 5px 10px #facc15a2',
+        'yellow': '1px 2px 3px #facc15a2',
+        'orange': '0 2px 3px #f97316a2',
+      },
+      scale:{
+        '.99': '.99',
       }
     },
   },
