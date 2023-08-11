fbba439dd2c1717e4ae9b6a200e833395fa3cb70
Berwin Singh <63018717+berwinsingh@users.noreply.github.com>
2023-08-12 01:42:25 +0530

    Completed intial HTML and CSS. Started working with JS

--- JavaScript/main.js
+++
@@ -1,3 +0,0 @@
-import { compareAsc, format } from 'date-fns'; //Helps in modifying dates and times in a better way
-
-format(new Date(11, 1, 2023), 'dd-MM-yyyy');
---
+++ JavaScript/new_task.js
@@ -0,0 +1,4 @@
+const addNewTask = document.getElementById("new-btn"); //Plus icon that will reveal the popup when clicked
+const newTaskPopup = document.getElementById("add-new-task"); //This will be shown or hidden on the click of the add btn
+const taskName = document.getElementById("task-name");
+const createTaskBtn = document.getElementById("create-task-btn");
